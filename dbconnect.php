e392c4a55c41b2725c7a472fd0b477a1b761bb7c
Yu Yasuda <tinohitorimono@yahoo.co.jp>
2025-07-17 17:35:44 +0900

    パスワード変更追加

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 <?php
     try {
         $db = new PDO('mysql:dbname=mini_bbs; host=127.0.0.1;charset=utf8', 'bbs_user', 'joZ1sPow1triB+ocr6di'); //データソース名、ユーザー名、パスワードの順に書く
-=======
-<?php 
-try {
-        $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;charset=utf8', 'root','');
->>>>>>> e10f27e20df9ddc13f771ba39afad4e985f9a9aa
     } catch (PDOException $e) {
         echo 'DB接続エラー: ' . $e->getMessage();
     }
