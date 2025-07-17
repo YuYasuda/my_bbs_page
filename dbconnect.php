5d4ed7b30e46e5a9f87253747dafc3467dcddb95
Yu Yasuda <tinohitorimono@yahoo.co.jp>
2025-07-17 17:05:56 +0900

    データベースの追加

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $db = new PDO('mysql:dbname=mini_bbs; host=127.0.0.1;charset=utf8', 'root', ''); //データソース名、ユーザー名、パスワードの順に書く
+        $db = new PDO('mysql:dbname=mini_bbs; host=127.0.0.1;charset=utf8', 'bbs_user', 'joZ1sPow1triB+ocr6di'); //データソース名、ユーザー名、パスワードの順に書く
     } catch (PDOException $e) {
         echo 'DB接続エラー: ' . $e->getMessage();
     }
---
+++ goods.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2024-10-23 14:36:08
+-- サーバのバージョン： 10.4.32-MariaDB
+-- PHP のバージョン: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `mini_bbs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `goods`
+--
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
